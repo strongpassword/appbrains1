e8c897f62005e45ecd84c4d9d006410e9ee2021f
strongpassword <gilelamp9611@granitesd.org>
2022-05-07 20:09:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
+class main():
+  
+ def __init__(self,field, o, timer, ttt, x)
+   self.field=field
+   self.o=o
+   self.timer=timer
+   self.ttt=ttt
+   self.x=x
+    
+class field(): #field is gameboard!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  def__init__(self, width, length)
+  width=15
+  length=15
+  self.width=width
+  self.length=length
+  def compute_area(self):
+    return self.width*self.length
+  def compute_perimeter(self)
+  return 2*(self.width+self.length)
 
+class o(): 
+   def: when keyPressed(insert(o))
+   keyboard.press_and_release("shift+o, space")
+    keyboard.write("O")
+    
+class.timer():
+  gameOver=timer.reset
+  
+class.ttt():
+  py_compile.compile(file, cfile=None, dfile=None, doraise=False, optimize=- 1, invalidation_mode=PycInvalidationMode.TIMESTAMP, quiet=0)
+    #src.file compiler
+    
+    
+class x(): 
+   def: when keyPressed(insert(x))
+   keyboard.press_and_release("shift+x, space")
+    keyboard.write("X")
+    
+   
+      
